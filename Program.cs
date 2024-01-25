343f053210c5f795333464f224c801e6018fac73
Kiril <kiril.chernysh@gmail.com>
2024-01-26 00:40:41 +0200

    combined letters to spell words correctly

--- Program.cs
+++ Program.cs
@@ -3,35 +3,92 @@ using System.Collections.Generic;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 
+
+
+
 namespace ExtractTextWithFontSize
 {
+    public class CustomTextChunk
+    {
+        public string Text { get; set; }
+        public float FontSize { get; set; }
+        public Vector StartLocation { get; set; }
+        public Vector EndLocation { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
             string pdfPath = "/Users/admin/Downloads/5ECA7D7D89C05126A1191A3AF0B2D417.pdf"; // Replace with the path to your PDF file
             var fontSizes = new HashSet<float>();
+            var strategy = new MyLocationTextExtractionStrategy(fontSizes);
+
+            List<CustomTextChunk> processedTextChunks = new List<CustomTextChunk>();
+
 
             using (PdfReader reader = new PdfReader(pdfPath))
             {
                 for (int i = 1; i <= reader.NumberOfPages; i++)
                 {
-                    ITextExtractionStrategy strategy = new MyLocationTextExtractionStrategy(fontSizes);
-                    string pageText = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
-                    Console.WriteLine($"Page {i}: \n{pageText}");
+                    PdfTextExtractor.GetTextFromPage(reader, i, strategy);
                 }
+
+            };
+
+            int wordPositionIndex = 0;
+
+            for (int i = 0; i < strategy.TextChunks.Count; i++)
+            {
+                var currentTextChunk = strategy.TextChunks[i];
+                if (i != 0)
+                {
+                    // Console.WriteLine($"i: {i}, wordPositionIndex:{wordPositionIndex}");
+                    if (currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1] < 1 && processedTextChunks[wordPositionIndex].FontSize == currentTextChunk.FontSize)
+                    {
+                        Console.WriteLine($"currentTextChunk.EndLocation[Vector.I1] - processedTextChunks[wordPositionIndex].StartLocation[Vector.I1]: {currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1]}");
+
+                        var joinText = new CustomTextChunk
+                        {
+                            Text = processedTextChunks[wordPositionIndex].Text + currentTextChunk.Text,
+                            FontSize = currentTextChunk.FontSize,
+                            StartLocation = processedTextChunks[wordPositionIndex].StartLocation,
+                            EndLocation = currentTextChunk.EndLocation,
+                            X = processedTextChunks[wordPositionIndex].X,
+                            Y = processedTextChunks[wordPositionIndex].Y
+                        };
+                        processedTextChunks[wordPositionIndex] = joinText;
+                    }
+                    else
+                    {
+
+                        wordPositionIndex++;
+                        processedTextChunks.Add(currentTextChunk);
+                    }
+                }
+                else
+                {
+                    // Console.WriteLine($"i: {i}, wordPositionIndex:{wordPositionIndex}");
+                    wordPositionIndex = i;
+                    processedTextChunks.Add(currentTextChunk);
+
+                }
+
             }
 
-            Console.WriteLine("Unique Font Sizes:");
-            foreach (var size in fontSizes)
+            foreach (var customTextChunk in processedTextChunks)
             {
-                Console.WriteLine(size);
+                Console.WriteLine($"Text: {customTextChunk.Text}, Font Size: {customTextChunk.FontSize}, Start Location: {customTextChunk.StartLocation}, End Location: {customTextChunk.EndLocation}");
             }
         }
     }
 
     public class MyLocationTextExtractionStrategy : LocationTextExtractionStrategy
     {
+
+        public List<CustomTextChunk> TextChunks { get; } = new List<CustomTextChunk>();
         private readonly HashSet<float> _fontSizes;
 
         public MyLocationTextExtractionStrategy(HashSet<float> fontSizes)
@@ -52,7 +109,19 @@ namespace ExtractTextWithFontSize
 
             _fontSizes.Add(fontSize);
 
-            Console.WriteLine($"Text: {currentText}, Font Size: {fontSize}, Start: {start}, End: {end}, X: {x}, Y: {y}");
+            var textChunk = new CustomTextChunk
+            {
+                Text = renderInfo.GetText(),
+                FontSize = renderInfo.GetAscentLine().GetStartPoint()[Vector.I2] - renderInfo.GetDescentLine().GetStartPoint()[Vector.I2],
+                StartLocation = renderInfo.GetBaseline().GetStartPoint(),
+                EndLocation = renderInfo.GetBaseline().GetEndPoint(),
+                X = bottomLeft[Vector.I1],
+                Y = bottomLeft[Vector.I2]
+            };
+
+            TextChunks.Add(textChunk);
+
+            // Console.WriteLine($"Text: {currentText}, Font Size: {fontSize}, Start: {start}, End: {end}, X: {x}, Y: {y}");
         }
     }
 }
