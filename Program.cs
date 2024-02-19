184bc2748d205551ed8098d58b45a1c083bff5e3
Kiril <kiril.chernysh@gmail.com>
2024-02-19 13:21:46 +0200

    added functionality for adding bookmarks

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using iTextSharp.text;
 
 
 
@@ -16,6 +18,7 @@ namespace ExtractTextWithFontSize
         public Vector EndLocation { get; set; }
         public float X { get; set; }
         public float Y { get; set; }
+        public int PageNumber { get; set; }
     }
 
     public class HierarchicalTextChunk
@@ -48,80 +51,110 @@ namespace ExtractTextWithFontSize
         {
 
             string pdfPath = "/Users/admin/Downloads/5ECA7D7D89C05126A1191A3AF0B2D417.pdf"; // Replace with the path to your PDF file
-            var fontSizes = new HashSet<float>();
-            var strategy = new MyLocationTextExtractionStrategy(fontSizes);
+            string newPdfPath = "/Users/admin/Downloads/5ECA7D7D89C05126A1191A3AF0B2D417_new.pdf";
+
 
             List<CustomTextChunk> processedTextChunks = new List<CustomTextChunk>();
-            
 
-            using (PdfReader reader = new PdfReader(pdfPath))
+            CreatePdfCopyWithBookmarks(pdfPath, newPdfPath);
+
+
+            using (PdfReader reader = new PdfReader(newPdfPath))
             {
                 for (int i = 1; i <= reader.NumberOfPages; i++)
                 {
+                    var fontSizes = new HashSet<float>();
+                    var strategy = new MyLocationTextExtractionStrategy(fontSizes, i);
                     PdfTextExtractor.GetTextFromPage(reader, i, strategy);
+                    processedTextChunks.AddRange(strategy.TextChunks);
                 }
 
             };
 
             int wordPositionIndex = 0;
 
-            for (int i = 0; i < strategy.TextChunks.Count; i++)
+            for (int i = 1; i < processedTextChunks.Count; i++)
             {
-                var currentTextChunk = strategy.TextChunks[i];
-                if (i != 0)
+                // Console.WriteLine(processedTextChunks[i].Text);
+                var currentTextChunk = processedTextChunks[i];
+                // Console.WriteLine($"i: {i}, wordPositionIndex:{wordPositionIndex}");
+                if (currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1] < 1)
                 {
-                    // Console.WriteLine($"i: {i}, wordPositionIndex:{wordPositionIndex}");
-                    if (currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1] < 1 && processedTextChunks[wordPositionIndex].FontSize == currentTextChunk.FontSize)
+                    // Console.WriteLine($"currentTextChunk.EndLocation[Vector.I1] - processedTextChunks[wordPositionIndex].StartLocation[Vector.I1]: {currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1]}");
+                    // Console.WriteLine($"processedTextChunks[wordPositionIndex].Text: {processedTextChunks[wordPositionIndex].Text}, currentTextChunk.Text: {currentTextChunk.Text}");
+                    var joinText = new CustomTextChunk
                     {
-                        // Console.WriteLine($"currentTextChunk.EndLocation[Vector.I1] - processedTextChunks[wordPositionIndex].StartLocation[Vector.I1]: {currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1]}");
+                        Text = processedTextChunks[wordPositionIndex].Text + "" + currentTextChunk.Text,
+                        FontSize = currentTextChunk.FontSize,
+                        StartLocation = processedTextChunks[wordPositionIndex].StartLocation,
+                        EndLocation = currentTextChunk.EndLocation,
+                        X = processedTextChunks[wordPositionIndex].X,
+                        Y = processedTextChunks[wordPositionIndex].Y,
+                        PageNumber = currentTextChunk.PageNumber
+                    };
+                    processedTextChunks[wordPositionIndex] = joinText;
+                    // Console.WriteLine($"currentTextChunk.Text: {currentTextChunk.Text}");
+                    processedTextChunks.RemoveAt(i);
+                    i--;
+                    // Console.WriteLine($"currentTextChunk.Text After Deliting: {processedTextChunks[i].Text}");
+                    // Console.WriteLine($"processedTextChunks[wordPositionIndex].Text AFTER: {processedTextChunks[wordPositionIndex].Text}");
 
-                        var joinText = new CustomTextChunk
-                        {
-                            Text = processedTextChunks[wordPositionIndex].Text + currentTextChunk.Text,
-                            FontSize = currentTextChunk.FontSize,
-                            StartLocation = processedTextChunks[wordPositionIndex].StartLocation,
-                            EndLocation = currentTextChunk.EndLocation,
-                            X = processedTextChunks[wordPositionIndex].X,
-                            Y = processedTextChunks[wordPositionIndex].Y
-                        };
-                        processedTextChunks[wordPositionIndex] = joinText;
-                    }
-                    else if (processedTextChunks[wordPositionIndex].FontSize == currentTextChunk.FontSize)
+                }
+                else if (processedTextChunks[wordPositionIndex].FontSize == currentTextChunk.FontSize)
+                {
+                    // Console.WriteLine($"processedTextChunks[wordPositionIndex].Text: {processedTextChunks[wordPositionIndex].Text}, currentTextChunk.Text: {currentTextChunk.Text}");
+
+                    var joinText = new CustomTextChunk
                     {
-                        var joinText = new CustomTextChunk
-                        {
-                            Text = processedTextChunks[wordPositionIndex].Text + ' ' + currentTextChunk.Text,
-                            FontSize = currentTextChunk.FontSize,
-                            StartLocation = processedTextChunks[wordPositionIndex].StartLocation,
-                            EndLocation = currentTextChunk.EndLocation,
-                            X = processedTextChunks[wordPositionIndex].X,
-                            Y = processedTextChunks[wordPositionIndex].Y
-                        };
-                        processedTextChunks[wordPositionIndex] = joinText;
-                    }
-                    else
-                    {
-                        wordPositionIndex++;
-                        processedTextChunks.Add(currentTextChunk);
-                    }
+                        Text = processedTextChunks[wordPositionIndex].Text + ' ' + currentTextChunk.Text,
+                        FontSize = currentTextChunk.FontSize,
+                        StartLocation = processedTextChunks[wordPositionIndex].StartLocation,
+                        EndLocation = currentTextChunk.EndLocation,
+                        X = processedTextChunks[wordPositionIndex].X,
+                        Y = processedTextChunks[wordPositionIndex].Y,
+                        PageNumber = currentTextChunk.PageNumber
+                    };
+                    processedTextChunks[wordPositionIndex] = joinText;
+                    // Console.WriteLine($"processedTextChunks[wordPositionIndex].Text AFTER: {processedTextChunks[wordPositionIndex].Text}");
+                    // Console.WriteLine($"currentTextChunk.Text: {currentTextChunk.Text}");
+                    processedTextChunks.RemoveAt(i);
+                    i--;
+                    // Console.WriteLine($"currentTextChunk.Text After Deliting: {processedTextChunks[i].Text}");
                 }
                 else
                 {
-                    // Console.WriteLine($"i: {i}, wordPositionIndex:{wordPositionIndex}");
-                    processedTextChunks.Add(currentTextChunk);
-
+                    // Console.WriteLine($"processedTextChunks[wordPositionIndex].Text: {processedTextChunks[wordPositionIndex].Text}, currentTextChunk.Text: {currentTextChunk.Text}");
+                    // Console.WriteLine($"currentTextChunk.EndLocation[Vector.I1] - processedTextChunks[wordPositionIndex].StartLocation[Vector.I1]: {currentTextChunk.StartLocation[Vector.I1] - processedTextChunks[wordPositionIndex].EndLocation[Vector.I1]}");
+                    wordPositionIndex++;
                 }
+
 
             }
 
-            // foreach (var customTextChunk in processedTextChunks)
+
+            // Console.WriteLine($"wordPositionIndex:{wordPositionIndex}, processedTextChunks.Count:{processedTextChunks.Count}");
+
+            // foreach (CustomTextChunk chunk in processedTextChunks)
             // {
-            //     Console.WriteLine($"Text: {customTextChunk.Text}, Font Size: {customTextChunk.FontSize}, Start Location: {customTextChunk.StartLocation}, End Location: {customTextChunk.EndLocation}");
+            //     Console.WriteLine($"Text: {chunk.Text}, Font Size: {chunk.FontSize}, Start Location: {chunk.StartLocation}, End Location: {chunk.EndLocation}");
             // }
 
             HierarchicalTextChunk treeRoot = BuildTree(processedTextChunks);
+            HashSet<int> uniqueFontSizes = GetUniqueFontSizes(treeRoot);
+            List<int> sortedFontSizes = uniqueFontSizes.ToList();
+            sortedFontSizes.Sort();
+            sortedFontSizes.Reverse();
 
-            PrintTree(treeRoot);
+            Console.WriteLine("Sorted unique font sizes:");
+            foreach (int fontSize in sortedFontSizes)
+            {
+                Console.WriteLine(fontSize);
+            }
+
+            Program programInstance = new Program();
+            programInstance.AddBookmarksToExistingPdf(newPdfPath, treeRoot); // Assuming treeRoot is defined
+
+            // PrintTree(treeRoot);
 
             static HierarchicalTextChunk BuildTree(List<CustomTextChunk> processedTextChunks)
             {
@@ -132,10 +165,11 @@ namespace ExtractTextWithFontSize
                     StartLocation = new Vector(0, 0, 0),
                     EndLocation = new Vector(0, 0, 0),
                     X = 0,
-                    Y = 0
+                    Y = 0,
+                    PageNumber = 0
                 }; // Placeholder root chunk
                 HierarchicalTextChunk root = new HierarchicalTextChunk(rootChunk);
-                
+
                 Console.WriteLine($"CurrentRoot:{HierarchicalTextChunk.CurrentRoot.TextChunk.Text}");
 
                 foreach (var chunk in processedTextChunks)
@@ -153,7 +187,7 @@ namespace ExtractTextWithFontSize
                 if (node.TextChunk.FontSize - newChunk.FontSize >= 2)
                 {
                     // Console.WriteLine($"Text:{node.TextChunk.Text},FontSize:{node.TextChunk.FontSize},newChunk.FontSize:{newChunk.FontSize}");
-                
+
                     // The new chunk is a child of the current node
                     node.AddChild(newChunk);
                 }
@@ -163,7 +197,7 @@ namespace ExtractTextWithFontSize
                     if (node.Parent != null)
                     {
                         // Console.WriteLine($"node.ParentText:{node.TextChunk.Text},FontSize:{node.TextChunk.FontSize},newChunk.FontSize:{newChunk.FontSize}");
-                
+
                         // There is a parent, so check if the new chunk should be inserted relative to the parent
                         InsertChunkRelativeToParent(node.Parent, newChunk);
                     }
@@ -191,6 +225,107 @@ namespace ExtractTextWithFontSize
             }
         }
 
+        public static HashSet<int> GetUniqueFontSizes(HierarchicalTextChunk root)
+        {
+            HashSet<int> uniqueFontSizes = new HashSet<int>();
+
+            void Traverse(HierarchicalTextChunk node)
+            {
+                // Add the current node's font size to the HashSet
+                uniqueFontSizes.Add(node.TextChunk.FontSize);
+
+                // Recursively traverse the children
+                foreach (var child in node.Children)
+                {
+                    Traverse(child);
+                }
+            }
+
+            // Start the traversal from the root
+            Traverse(root);
+
+            return uniqueFontSizes;
+        }
+
+        static void CreatePdfCopyWithBookmarks(string sourcePdfPath, string destinationPdfPath)
+        {
+            using (PdfReader reader = new PdfReader(sourcePdfPath))
+            {
+                using (FileStream fs = new FileStream(destinationPdfPath, FileMode.Create, FileAccess.Write))
+                {
+                    using (Document document = new Document(reader.GetPageSizeWithRotation(1)))
+                    {
+                        PdfCopy copy = new PdfCopy(document, fs);
+                        document.Open();
+
+                        // Copy each page from the source PDF to the new PDF
+                        for (int i = 1; i <= reader.NumberOfPages; i++)
+                        {
+                            copy.AddPage(copy.GetImportedPage(reader, i));
+                        }
+
+                        document.Close();
+
+                        // Now, add bookmarks based on the hierarchical tree
+                        // This step must be done after the document is closed
+                    }
+                }
+            }
+        }
+
+
+        public void AddBookmarksToExistingPdf(string pdfPath, HierarchicalTextChunk rootNode)
+        {
+            string tempFilePath = pdfPath + ".tmp";
+            using (PdfReader reader = new PdfReader(pdfPath))
+            {
+                // Create a temporary file stream for the output to avoid issues with reading and writing to the same file simultaneously
+
+                using (FileStream fs = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    using (PdfStamper stamper = new PdfStamper(reader, fs))
+                    {
+                        IList<Dictionary<string, object>> bookmarks = new List<Dictionary<string, object>>();
+                        AddBookmarksFromNode(rootNode, bookmarks);
+
+                        // Apply the bookmarks to the stamper
+                        stamper.Outlines = bookmarks;
+                    }
+                }
+
+                // Close the reader to release the original file
+                reader.Close();
+            }
+
+            // Replace the original file with the modified temporary file
+            File.Delete(pdfPath);
+            File.Move(tempFilePath, pdfPath);
+        }
+
+        private void AddBookmarksFromNode(HierarchicalTextChunk node, IList<Dictionary<string, object>> bookmarksList)
+        {
+            Dictionary<string, object> bookmark = new Dictionary<string, object>
+    {
+        { "Title", node.TextChunk.Text },
+        { "Action", "GoTo" },
+        // This assumes the X, Y coordinates and page number are set correctly in your CustomTextChunk
+        { "Page", $"{node.TextChunk.PageNumber} XYZ {node.TextChunk.X} {node.TextChunk.Y} null" }
+    };
+            Console.WriteLine($"Title: {node.TextChunk.Text}");
+            bookmarksList.Add(bookmark);
+
+            if (node.Children.Count > 0 && node.TextChunk.FontSize >= 9)
+            {
+                List<Dictionary<string, object>> kids = new List<Dictionary<string, object>>();
+                foreach (HierarchicalTextChunk child in node.Children)
+                {
+                    AddBookmarksFromNode(child, kids);
+                }
+                bookmark.Add("Kids", kids);
+            }
+        }
+
+
     }
 
 
@@ -199,18 +334,17 @@ namespace ExtractTextWithFontSize
 
         public List<CustomTextChunk> TextChunks { get; } = new List<CustomTextChunk>();
         private readonly HashSet<float> _fontSizes;
+        private int _pageNumber; // Store the current page number
 
-        public MyLocationTextExtractionStrategy(HashSet<float> fontSizes)
+        public MyLocationTextExtractionStrategy(HashSet<float> fontSizes, int pageNumber)
         {
             _fontSizes = fontSizes;
+            _pageNumber = pageNumber;
         }
         public override void RenderText(TextRenderInfo renderInfo)
         {
             base.RenderText(renderInfo);
 
-            string currentText = renderInfo.GetText(); // Get the text
-            Vector start = renderInfo.GetBaseline().GetStartPoint();
-            Vector end = renderInfo.GetBaseline().GetEndPoint();
             float fontSize = renderInfo.GetAscentLine().GetStartPoint()[Vector.I2] - renderInfo.GetDescentLine().GetStartPoint()[Vector.I2];
             Vector bottomLeft = renderInfo.GetDescentLine().GetStartPoint();
             float x = bottomLeft[Vector.I1];
@@ -225,7 +359,8 @@ namespace ExtractTextWithFontSize
                 StartLocation = renderInfo.GetBaseline().GetStartPoint(),
                 EndLocation = renderInfo.GetBaseline().GetEndPoint(),
                 X = bottomLeft[Vector.I1],
-                Y = bottomLeft[Vector.I2]
+                Y = bottomLeft[Vector.I2],
+                PageNumber = _pageNumber
             };
 
             TextChunks.Add(textChunk);
